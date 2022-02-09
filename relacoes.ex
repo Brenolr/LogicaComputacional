5719c91e06307ac37ed0636add1ecefa26b9d8d3
BrenoLRocha <blrocha@stefanini.com>
2022-02-09 20:52:43 -0300

    CABOOOOOOOOOOOOO CABBBBBBBBBOOOOOOOOOOOOOOOOOOO CABOOOOOOOOOOOOOO

--- relacoes.ex
+++ relacoes.ex
@@ -1,68 +1,61 @@
 defmodule Relacoes do
-  # def compor(a,b) do
-  #     var = Enum.map(a,
-  #         fn [a0,a1] -> Enum.map(b,
-  #             fn [b0,b1] -> if(a1==b0) do
-  #                     [a0,b1]
-  #                 end
-  #             end
-  #             )
-  #         end
-  #         )
-  #     IO.inspect(var)
-  #     var2 = Enum.reduce(var, fn item, acc ->
-  #       IO.inspect(item)
-  #       [item|acc] end)
-  #     Enum.reduce(var2, fn item, acc -> if item != nil do
-  #       [item|acc]
-  #     else
-  #       acc
-  #     end
-  #   end)
-  # end
-  def compor(a, b) do
-    acc = []
-    var =
-      Enum.map_reduce(
-        a, acc,
-        fn el, acc ->
-          Enum.map(b, fn el2 ->
-            if(a1 == b0) do
-              [[a0, b1] | acc]
-            end
-          end)
-        end
-      )
-
-    IO.inspect(var)
+  def compose(a, b) do
+    compose(a, b, 0, 0, [])
   end
 
-  # def compor(a,b) do
-  #     r = []
-  #     Enum.each(a,
-  #         fn [a0,a1] -> Enum.each(b,
-  #             fn [b0,b1] -> if(a1==b0) do
-  #                 r = [[a0,b1]|r]
-  #                 IO.inspect([a0,b1])
-  #                 IO.inspect(r)
-  #                 end
-  #             end
-  #             )
-  #         end
-  #         )
+  def compose(a, b, ai, bi, acc) do
+    # IO.inspect([a, b, ai, bi, acc])
+    # IO.inspect(ai < Enum.count(a))
 
-  # end
-  def rec(0, r, rn, u), do: u
+    if(ai < Enum.count(a)) do
+      aa = Enum.at(a, ai)
+      bb = Enum.at(b, bi)
+      # IO.inspect([a, b, ai, bi, acc, aa, bb])
 
-  def rec(n, r, rn, u) do
-    novo_rn = compor(r, rn)
-    update_u = MapSet.union(u, novo_rn)
-    rec(n - 1, r, novo_rn, update_u)
+      if Enum.at(aa, 1) == Enum.at(bb, 0) do
+        new = [Enum.at(aa, 0), Enum.at(bb, 1)]
+        acc2 = acc ++ [new]
+        # IO.inspect(acc2)
+        # IO.inspect("teste 1")
+
+        if(bi == Enum.count(b) - 1) do
+          compose(a, b, ai + 1, 0, acc2)
+        else
+          compose(a, b, ai, bi + 1, acc2)
+        end
+      else
+        # IO.inspect([ai,bi])
+        if(bi == Enum.count(b) - 1) do
+          compose(a, b, ai + 1, 0, acc)
+        else
+          compose(a, b, ai, bi + 1, acc)
+        end
+      end
+    else
+      acc
+    end
+  end
+  def rec(0, d, _r, _rn, acc) do
+    dup = Enum.map(d, fn x-> [x, x] end)
+    Enum.uniq((acc ++ dup))
+  end
+
+
+  def rec(n, d, r, rn, acc) do
+    novo_rn = compose(r,rn)
+    update_acc = Enum.uniq((acc ++ novo_rn))
+    rec(n - 1, d, r, novo_rn, update_acc)
+  end
+  def alg(d,r) do
+    rec(Enum.count(d), d, r, r, r)
   end
 end
 
-a = MapSet.new([[1, 2], [3, 2], [2, 2]])
-b = MapSet.new([[1, 2], [2, 1], [1, 3]])
+# a = MapSet.new([[1, 2], [3, 2], [2, 2], [4, 1]])
+# b = MapSet.new([[1, 2], [2, 1], [1, 3]])
+c = [[1, 1],[1, 2],[2, 3]]
 # IO.inspect(MapSet.union(a,b))
 # [[1,2],[3,1],[2,3]]
-IO.inspect(Relacoes.compor(a, b))
+# IO.inspect(Relacoes.compose(a, b))
+# IO.inspect(Relacoes.rec(3, c, c, c))
+IO.inspect(Relacoes.alg([1,2,3,4,5], c))
