6c92a74383041e7919e8009e9da694ebcd5db46f
BrenoLRocha <blrocha@stefanini.com>
2022-03-08 12:45:21 -0300

    reconfiguração e criação do EP2

---
+++ Ep2/ep2/lib/cadeias.ex
@@ -0,0 +1,18 @@
+defmodule Cadeias do
+  @moduledoc """
+  Documentation for `Cadeias`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cadeias.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ Ep2/ep2/test/cadeias_test.exs
@@ -0,0 +1,8 @@
+defmodule CadeiasTest do
+  use ExUnit.Case
+  doctest Cadeias
+
+  test "greets the world" do
+    assert Cadeias.hello() == :world
+  end
+end
