c641f3bc803024a14beea3ed843ae9e3afd271f8
BrenoLRocha <blrocha@stefanini.com>
2022-03-09 09:19:47 -0300

    update

--- Ep2/ep2/lib/cadeias.ex
+++ Ep2/ep2/lib/cadeias.ex
@@ -1,18 +1,15 @@
 defmodule Cadeias do
-  @moduledoc """
-  Documentation for `Cadeias`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cadeias.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def gerar_vocab(gram, size) do
+    gram == size
+  end
+  def tem_maius(palavra) do
+    palavra_s = List.to_string(palavra)
+    palavra_s != String.downcase(palavra_s)
   end
 end
+# IO.inspect(is_list('ab'))
+# IO.inspect('ab' == 'abc')
+teste = Cadeias.tem_maius('eno')
+IO.inspect(teste)
+grammar = [['A', 'a'],['B', 'b'],['C', 'c']]
+Cadeias.gerar_vocab(grammar,1)
