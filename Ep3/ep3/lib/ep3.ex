ea9fc7a86140d75eb1d445b982e3f803b68b0f49
BrenoLRocha <blrocha@stefanini.com>
2022-03-30 12:43:11 -0300

    iniciar ep3

---
+++ Ep3/ep3/lib/ep3.ex
@@ -0,0 +1,18 @@
+defmodule Ep3 do
+  @moduledoc """
+  Documentation for `Ep3`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ep3.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ Ep3/ep3/test/ep3_test.exs
@@ -0,0 +1,8 @@
+defmodule Ep3Test do
+  use ExUnit.Case
+  doctest Ep3
+
+  test "greets the world" do
+    assert Ep3.hello() == :world
+  end
+end
