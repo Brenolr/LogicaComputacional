324ad234352372d4ea9caeddb7b44825fa55e45b
BrenoLRocha <blrocha@stefanini.com>
2022-04-03 18:55:41 -0300

    arrumamos o Ex 2

--- Ep3/ep3/test/ep3_test.exs
+++ Ep3/ep3/test/ep3_test.exs
@@ -11,16 +11,19 @@ defmodule MaquinaDeterministicaTest do
   end
   test "teste2" do
     tape = [:b]
-                    }
     state_graph = %{p: %{a: [:p], b: [:p, :q], is_accept_state: false},
-                    q: %{a: [:q], b: [:q], is_accept_state: true}
+                    q: %{a: [:q], b: [:q], is_accept_state: true}}
     assert Machines.nonDeterministicMachine(tape, state_graph, :p, 0) == true
   end
-  test "teste2" do
-    tape = [:b]
-    state_graph = %{p: %{a: [:p], b: [:p, :q], is_accept_state: false},
-                    q: %{a: [:q], b: [:q], is_accept_state: true}
+  test "teste3" do
+    tape = [:a,:a, :b, :a]
+    state_graph = %{s1: %{a: [:s2,:s3], b: [:s1, :s2], is_accept_state: false},
+                    s2: %{a: [:s4,:s5], b: [:s2], is_accept_state: true},
+                    s3: %{a: [:s3], b: [:s3], is_accept_state: false},
+                    s4: %{a: [:s5], b: [:s2], is_accept_state: false},
+                    s5: %{a: [:s5], b: [:s5], is_accept_state: false}
                     }
-    assert Machines.nonDeterministicMachine(tape, state_graph, :p, 0) == true
+    assert Machines.nonDeterministicMachine(tape, state_graph, :s1, 0) == false
   end
+
 end
